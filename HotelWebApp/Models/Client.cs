df91ebc30c61637074eeff6b05768c7c1f5d63ab
Yura Askerko <yraskerko3636@gmail.com>
2020-10-13 20:43:04 +0300

    fwe

---
+++ HotelWebApp/Models/Client.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HotelWebApp.Models
+{
+    public partial class Client
+    {
+        public Client()
+        {
+            Orders = new HashSet<Order>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Middlename { get; set; }
+        public string Surname { get; set; }
+        public string PassportData { get; set; }
+        public string NameOfRoom { get; set; }
+        public string List { get; set; }
+        public decimal? TotalCost { get; set; }
+
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+}
---
+++ HotelWebApp/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HotelWebApp.Models
+{
+    public partial class Order
+    {
+        public int Id { get; set; }
+        public DateTime? CheckInDate { get; set; }
+        public DateTime? CheckOut { get; set; }
+        public int EmployeeId { get; set; }
+        public int ClientId { get; set; }
+        public int RoomId { get; set; }
+
+        public virtual Client Client { get; set; }
+        public virtual Employee Employee { get; set; }
+        public virtual Room Room { get; set; }
+    }
+}
